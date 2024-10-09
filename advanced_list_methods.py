6208518d28ea94efe1f1da2d51aa677ea6e7c506
Emanuel Garcia <Emanuelfgarcia89@gmail.com>
2024-10-09 14:19:08 -0400

    module 2 lesson 3 assignments

---
+++ advanced_list_methods.py
@@ -0,0 +1,5 @@
+#Find out if Alice submitted their assignment and attended class. 
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+if "Alice" in submitted and attended:
+    print("Alice attended class and submitted her assignment")
---
+++ python_list_transformation_task2.py
@@ -0,0 +1,6 @@
+#Calculate the average grade and print it.
+
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+
+average=sum(grades)/len(grades)
+print("average grade =", (average))
